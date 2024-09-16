a23c0b2b0493f2dec8078796e21ed38145f4b63e
kerolos-raouf <83724226+kerolos-raouf@users.noreply.github.com>
2024-09-16 19:42:24 +0300

    add dependency to the project

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.nimbusweatherapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.nimbusweatherapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+////more features in the activity like lazy initialization
+    implementation(libs.androidx.activity.ktx)
+////to use viewModel with fragment
+    implementation(libs.androidx.fragment.ktx)
+
+    //retrofit and gson converter
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+
+    ///glide dependency for images
+    implementation(libs.glide)
+
+
+
+///room database
+    val room_version = "2.6.1"
+
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+
+    ///open street map api
+    implementation(libs.osmdroid.android)
+
+    ///hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+}
